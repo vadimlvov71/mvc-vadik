11083bf1b219f6d75a7b4751f3256f9d38fedb26
Vadym Podolyan <vadym.podolyan@engineering.ait.com>
2020-07-28 20:19:56 +0300

    second commit

--- classes/Routing.php
+++ classes/Routing.php
@@ -63,30 +63,6 @@ class Routing {
 			echo "</pre>";
 		$controller = new $controllerName();
 		$controller->$actionName($arg);
-			echo "!!!!!";
-		/*Определяем контроллер*/
-		/*
-		if($route[1] != '') {
-			$controllerName = ucfirst($route[1]. "Controller");
-			$modelName = ucfirst($route[1]. "Model");
-		}
-
-
-		require_once CONTROLLER_PATH . $controllerName . ".php"; //IndexController.php
-		require_once MODEL_PATH . $modelName . ".php"; //IndexModel.php
-
-		if(isset($route[2]) && $route[2] !='') {
-			$action = $route[2];
-		}
-
-		$controller = new $controllerName();
-		$controller->$action(); // $controller->index();
-*/
+			
 	}
-	
-	public function errorPage() {
-
-	}
-
-
 }
